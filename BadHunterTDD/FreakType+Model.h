d247a1e7d67732aaa537ec52ebae4d15d08d0794
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2014-12-06 21:09:57 +0100

    Add first test and code for the freak type entity.
    Notice that only creation through the convenience constructor is tested, not data persistence.

---
+++ BadHunterTDD/FreakType+Model.h
@@ -0,0 +1,19 @@
+//
+//  FreakType+Model.h
+//  BadHunterTDD
+//
+//  Created by Jorge D. Ortiz Fuentes on 6/12/14.
+//  Copyright (c) 2014 PoWWaU. All rights reserved.
+//
+
+
+#import "FreakType.h"
+
+extern NSString *const freakTypeEntityName;
+
+
+@interface FreakType (Model)
+
++ (instancetype) freakTypeInMOC:(NSManagedObjectContext *)moc withName:(NSString *)name;
+
+@end
---
+++ BadHunterTDDTests/FreakTypeTests.m
@@ -0,0 +1,97 @@
+//
+//  FreakTypeTests.m
+//  BadHunterTDD
+//
+//  Created by Jorge D. Ortiz Fuentes on 6/12/14.
+//  Copyright (c) 2014 PoWWaU. All rights reserved.
+//
+
+
+#import <XCTest/XCTest.h>
+#import "FreakType+Model.h"
+
+
+@interface FreakTypeTests : XCTestCase {
+    // Core Data stack objects.
+    NSManagedObjectModel *model;
+    NSPersistentStoreCoordinator *coordinator;
+    NSPersistentStore *store;
+    NSManagedObjectContext *context;
+    // Object to test.
+    FreakType *sut;
+}
+
+@end
+
+
+@implementation FreakTypeTests
+
+#pragma mark - Set up and tear down
+
+- (void) setUp {
+    [super setUp];
+
+    [self createCoreDataStack];
+    [self createFixture];
+    [self createSut];
+}
+
+
+- (void) createCoreDataStack {
+    NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+    model = [NSManagedObjectModel mergedModelFromBundles:@[bundle]];
+    coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:model];
+    store = [coordinator addPersistentStoreWithType: NSInMemoryStoreType
+                                      configuration: nil
+                                                URL: nil
+                                            options: nil
+                                              error: NULL];
+    context = [[NSManagedObjectContext alloc] init];
+    context.persistentStoreCoordinator = coordinator;
+}
+
+
+- (void) createFixture {
+    // Test data
+}
+
+
+- (void) createSut {
+    sut = [FreakType freakTypeInMOC:context withName:nil];
+}
+
+
+- (void) tearDown {
+    [self releaseSut];
+    [self releaseFixture];
+    [self releaseCoreDataStack];
+
+    [super tearDown];
+}
+
+
+- (void) releaseSut {
+    sut = nil;
+}
+
+
+- (void) releaseFixture {
+
+}
+
+
+- (void) releaseCoreDataStack {
+    context = nil;
+    store = nil;
+    coordinator = nil;
+    model = nil;
+}
+
+
+#pragma mark - Basic test
+
+- (void) testObjectIsNotNil {
+    XCTAssertNotNil(sut, @"The object to test must be created in setUp.");
+}
+
+@end
